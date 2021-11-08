3787aa4282ab820bc69560f8cb33a899dfeb264b
Limei  Chen <limeichen@Limeis-MacBook-Air.local>
2021-11-08 13:12:36 -0500

    adding files for main tab bar

---
+++ FlashCards/FlashCards/FlashcardCell.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardCell.swift
+//  FlashCards
+//
+//  Created by Limei  Chen on 11/8/21.
+//
+
+import UIKit
+
+class FlashcardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
